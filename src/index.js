03eedd9337f1d9b6ede2d3f073907e602c5c0237
VISHVA G <vishvageo2004@gmail.com>
2025-10-29 21:15:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+"scripts": {
+  "start": "node index.js"
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
